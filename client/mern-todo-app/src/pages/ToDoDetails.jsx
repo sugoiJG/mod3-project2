105903694c684951850e03baa4b75ccff593d3e9
sugoiJG <jasongolliet@icloud.com>
2023-07-12 16:02:54 -0700

    added styling

--- client/mern-todo-app/src/pages/ToDoDetails.jsx
+++ client/mern-todo-app/src/pages/ToDoDetails.jsx
@@ -2,37 +2,39 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { TodoContext } from "../TodoContext/TodoContext";
 
-
-
-
 const TodoDetails = () => {
   const { id } = useParams();
   const [todo, setTodo] = useState(null);
 
-const { listItems } = useContext(TodoContext)
+  const { listItems } = useContext(TodoContext);
 
-
-useEffect(() => {
+  useEffect(() => {
     console.log("running use effect");
     const foundToDo = listItems.find((todo) => {
-      if(todo._id == id ) {
-      return true;
+      if (todo._id == id) {
+        return true;
       }
-})
-setTodo(foundToDo)
-
-},[])
-if (!todo) {
+    });
+    setTodo(foundToDo);
+  }, []);
+  if (!todo) {
     return <div className="flex justify-center text-center">Loading...</div>;
   }
   return (
     <div>
-        <h1 className="flex justify-center text-center text-2xl">To Do List</h1>
+      <h1 className="flex justify-center text-center text-2xl underline">
+        To Do List
+      </h1>
       <p className="flex justify-center text-center text-2xl">{todo.item}</p>
-      <p className="flex justify-center text-center text-2xl">Priority: {todo.priority}</p>
-      <p className="flex justify-center text-center text-2xl">Due Date: {todo.dueDate}</p>
+      <p className="flex justify-center text-center text-2xl">
+        Priority: {todo.priority}
+      </p>
+      <p className="flex justify-center text-center text-2xl">
+        Due Date: {new Date(todo.dueDate).toLocaleDateString('en-US')}
+      </p>
     </div>
   );
+  
 };
 
 export default TodoDetails;
--- server/models/toDoItems.js
+++ server/models/toDoItems.js
@@ -15,5 +15,19 @@ const TodoItemSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('TodoItem', TodoItemSchema);
+// Define a virtual property to format the dueDate as "Month/Day/Year"
+TodoItemSchema.virtual('formattedDueDate').get(function () {
+  // Check if the dueDate exists
+  if (this.dueDate) {
+    // Format the date as "Month/Day/Year"
+    return this.dueDate.toLocaleDateString('en-US');
+  }
+  
+  // Return null or any default value if the dueDate is not set
+  return null;
+});
 
+// Ensure that the virtual property is included when converting the document to JSON
+TodoItemSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('TodoItem', TodoItemSchema);
